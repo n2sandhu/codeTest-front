076631c92c75da74a271490ec19bce10e3c43179
n2sandhu <navi_sandhu2013@yahoo.com.au>
2017-03-14 17:19:46 +1100

    Arranging data in hierarchy as required

--- js/src/app.js
+++ js/src/app.js
@@ -1,20 +1,29 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute', 'ngResource']);
+var app = angular.module('app', ['ngRoute', 'ngResource', 'lbServices', 'angular.filter']);
 
-app.config(['$routeProvider',
-  function($routeProvider) {
+app
+    .config(['$routeProvider',
+      function($routeProvider) {
 
-    $routeProvider
-      .when('/', {
-        controller: 'mainController',
-        templateUrl: 'views/home.html'
-      }).when('/examples', {
-        templateUrl: 'views/examples.html',
-        controller: 'mainController'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }
-]);
+        $routeProvider
+          .when('/', {
+            controller: 'mainController',
+            templateUrl: 'views/home.html'
+          }).when('/examples', {
+            templateUrl: 'views/examples.html',
+            controller: 'mainController'
+          }).
+          otherwise({
+            redirectTo: '/'
+          });
+      }
+    ])
+    .config(function(LoopBackResourceProvider) {
+
+        // Use a custom auth header instead of the default 'Authorization'
+        LoopBackResourceProvider.setAuthHeader('X-Access-Token');
+
+        // Change the URL where to access the LoopBack REST API server
+        LoopBackResourceProvider.setUrlBase('http://localhost:3000/api');
+    });
--- js/src/controllers/mainController.js
+++ js/src/controllers/mainController.js
@@ -1,48 +1,99 @@
 'use strict';
 
-app.controller('mainController', ['$scope', 'DataService', 'employee', function($scope, DataService, employee) {
+app.controller('mainController', ['$scope', 'DataService', 'Employee', function($scope, DataService, Employee) {
 
-  // $scope.data = DataService.getData();
-  // employee.find({
-  //   filter: {
-  //     include: ['manager']
-  //   }
-  // })
-  // .$promise
-  // .then(function(employees) {
-  // $scope.employeesData = employees;
-  // });
+  $scope.managerData = [];
 
-    $scope.employeesData = [
-        {
-            id: 100,
-            name: 'Alan',
-            managerId: 150
-        },
-        {
-            id: 220,
-            name: 'Martin',
-            managerId: 100
-        },
-        {
-            id: 150,
-            name: 'Jamie'
-        },
-        {
-            id: 275,
-            name: 'Alex',
-            managerId: 100
-        },
-        {
-            id: 400,
-            name: 'Steve',
-            managerId: 150
-        },
-        {
-            id: 190,
-            name: 'David',
-            managerId: 400
-        }
-    ];
+  Employee.find({
+    filter: {
+      include: ['manager']
+    }
+  })
+  .$promise
+  .then(function(employees) {
+      $scope.employeesData = employees;
+      employees.forEach(function(employee, index) {
+          var employeeId = employee.id;
+          $scope.managerData[employee.id] = [];
+          arrangeData(employees, $scope.managerData[employee.id], employee, function (completed) {
+              if(completed == true && index == employees.length - 1) {
+                  //arrangeData2($scope.managerData);
+                  console.log('managerArray', $scope.managerData);
+
+
+                  $scope.employeesData.forEach(function(empy, ind) {
+                      if( empy.managerId) {
+                          delete $scope.employeesData[ind];
+                      }
+                      else {
+                          empy.position = 2;
+                          empy.employees.forEach(function(e) {
+                              if(e.isManager == true) {
+                                  var manager = e;
+                                  while(manager != null) {
+
+                                  }
+                              }
+                          });
+
+                      }
+                  });
+                  console.log('employeesArray', $scope.employeesData);
+              }
+          });
+      });
+  });
+
+  var arrangeData = function(employees, managerArray, employee, cb) {
+      employees.forEach(function(emp, index) {
+          if(emp.managerId == employee.id) {
+              employee.isManager = true;
+              managerArray.push(emp);
+          }
+          if (index == employees.length - 1) {
+              employee.employees = managerArray;
+              if(managerArray.length == 0) {
+                  employee.isManager = false;
+              }
+              cb(true);
+          }
+      });
+  };
+    /*
+  var arrangeData2 = function (managerArray) {
+      managerArray.forEach(function(managerData, index) {
+         console.log('ind', index);
+         managerData.forEach(function(employee) {
+            if( employee.id) {
+
+            }
+         });
+      });
+  };
+*/
+
+    // Employee.find({
+    //     filter: {
+    //         include: ['manager']
+    //     }
+    // })
+    //     .$promise
+    //     .then(function(employees) {
+    //         $scope.employeesData = employees;
+    //         $scope.managerHierarcy = [];
+    //         employees.forEach(function(employee, index) {
+    //             if(!employee.managerId) {
+    //                 $scope.managerHierarcy[employee.id] = [];
+    //                 employees.forEach(function(emp, index) {
+    //                     if(emp.managerId == employee.id) {
+    //                         $scope.managerHierarcy[employee.id].push(emp);
+    //                     }
+    //                 });
+    //             }
+    //             else {
+    //
+    //             }
+    //         });
+    //     });
 
 }]);
